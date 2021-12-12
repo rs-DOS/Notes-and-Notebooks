eaa70e5f848f6078b557ee424eb23e56ae426d19
Rushabh Doshi <rushabhdoshi94@gmail.com>
2021-12-12 05:58:20 +0530

    commit3

--- New_file.py
+++ New_file.py
@@ -1,1 +1,4 @@
-#This is a new python file to check if the commits are being pushed to GitHub
+def new_func():
+    #This is a new python file to check if the commits are being pushed to GitHub
+
+new_func()
